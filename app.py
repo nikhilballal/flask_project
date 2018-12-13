5fe353ccfccd193f9260c236072b06960a46660b
nikhilballal <nikhilballal.94@gmail.com>
2018-12-13 16:12:18 +0530

    basic flask

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/") #decorator
+def hello():
+	return "Hello World!"
+
+@app.route("/about")
+def about():
+	return "I am Ballal"
+
+@app.route("/about/contact")
+def contact():
+	return "phone number"
+
+
+if __name__=='__main__':
+	app.run()	
+
+
+
