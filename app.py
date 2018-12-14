bb83c0f584d45ecf3c885e333e14fc49c57a1dc9
nikhilballal <nikhilballal.94@gmail.com>
2018-12-14 13:48:37 +0530

    articles

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template
+from data import Articles # function from data.py
 # from module flask, we import the function Flask
 app = Flask(__name__)
+
+Articles = Articles() #create a variables equal to the function 'Articles' so that it can return the variable ' articles'
 
 @app.route("/")   #without this, we will get 'not found' page.
 def hello():
@@ -10,6 +13,27 @@ def hello():
 def about():
     return render_template("about.html")
 
+@app.route("/articles")
+def articles():
+    return render_template("articles.html" , articles = Articles) # since we want not only the html file, but also the data inside
+
+@app.route("/article/<string:id>/") #pass in string of values containing a parameter 'id'
+def article(id):
+    return render_template("article.html" , id=id)
+
 
 if __name__ == '__main__':
     app.run(debug = True) # so that we donot have to re-start the server
+
+
+
+
+
+
+
+#  to push a code in github from terminals
+# 1. git status
+# 2. git add {File_Name} //the file name you haven been changed
+# 3. git status
+# 4. git commit -m '{your_message}'
+# 5. git push origin master
---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [    #create a variable 'articles'
+    {
+        'id': 1,
+        'title':  'Article One',
+        'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'author': 'Nikhil Ballal',
+        'create date': '14-12-2018'
+    },
+    {
+        'id': 2,
+        'title':  'Article Two',
+        'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'author': 'Sonia Ballal',
+        'create date': '14-12-2018'
+    },
+    {
+        'id': 3,
+        'title':  'Article Three',
+        'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'author': 'Vishak Bharadwaj',
+        'create date': '14-12-2018'
+    }
+
+    ]
+    return articles
