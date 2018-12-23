cf7a822135b6a67d9dcf738bc9858a9aafb01c47
nikhilballal <nikhilballal.94@gmail.com>
2018-12-23 21:20:05 +0530

    loginII

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-from data import Articles # function from data.py
+#from data import Articles # function from data.py
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
@@ -18,7 +18,7 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  #queries in mysql can return dic
 #initialize MYSQL
 mysql = MySQL(app)
 
-Articles = Articles() #create a variables equal to the function 'Articles' so that it can return the variable ' articles'
+# Articles = Articles() #create a variables equal to the function 'Articles' so that it can return the variable ' articles'.
 
 #Index
 @app.route("/")   #without this, we will get 'not found' page.
@@ -32,15 +32,36 @@ def about():
 #Articles
 @app.route("/articles")
 def articles():
-    return render_template("articles.html" , articles = Articles) # since we want not only the html file, but also the data inside
+        #Create cursor
+        cur = mysql.connection.cursor()
+
+        #Get articles
+        result = cur.execute("SELECT * FROM articles")
+
+        articles = cur.fetchall()
+
+        if result> 0:
+            return render_template('articles.html', articles = articles)
+        else:
+            msg = 'No Articles Found'
+            return render_template('articles.html', msg=msg)
+        #Close connection
+        cur.close()
 
 #Single Article
 @app.route("/article/<string:id>/") #pass in string of values containing a parameter 'id'
 def article(id):
-    return render_template("article.html" , id=id)
+            #Create cursor
+            cur = mysql.connection.cursor()
+
+            #Get articles
+            result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+
+            article = cur.fetchone()
+            return render_template("article.html" , article=article)
 
 
-#Register Form Class
+#Register Form Classssss
 class RegisterForm(Form):
     name= StringField('Name', [validators.Length(min=1, max=50)])
     username =  StringField('Username', [validators.Length(min=4, max=25)])
@@ -76,7 +97,7 @@ def register():
 
         return redirect(url_for('index'))
 #to check if it is GET or POST request, and  make sure it is all validated
-    return render_template('register.html', form=form)
+    return render_template('register.html', form=form) # since we want not only the html file, but also the data inside
 
 #User Login
 @app.route('/login', methods=['GET','POST']) #to get both GET and POST requests
@@ -100,7 +121,7 @@ def login():
             #compare passwords
             if sha256_crypt.verify(password_candidate,password):
                 #Passed
-                session['logged in'] = True
+                session['logged_in'] = True
                 session['username'] = username
 
                 flash('You are now logged in', 'success')
@@ -130,16 +151,66 @@ def is_logged_in(f):
 
 #Logout
 @app.route('/logout')
+@is_logged_in
 def logout():
-    session.clear()
+    session.clear() #kill the session
     flash('You are now logged out', 'success')
     return redirect(url_for('login'))
 
 #Dashboard
 @app.route('/dashboard')
-@is_logged_in
+@is_logged_in  #indicate that this is page that the user has to be logged into.
 def dashboard():
-    return render_template('dashboard.html')
+    #Create cursor
+    cur = mysql.connection.cursor()
+
+    #Get articles
+    result = cur.execute("SELECT * FROM articles")
+
+    articles = cur.fetchall()
+
+    if result> 0:
+        return render_template('dashboard.html', articles = articles)
+    else:
+        msg = 'No Articles Found'
+        return render_template('dashboard.html', msg=msg)
+
+    #Close connection
+    cur.close()
+
+#Article Form Class
+class ArticleForm(Form):
+    title= StringField('Title', [validators.Length(min=1, max=200)])
+    body =  TextAreaField('Body', [validators.Length(min=30)])
+
+#Add Article
+@app.route('/add_article', methods= ['GET','POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        #Create Cursor
+        cur = mysql.connection.cursor()
+
+        #execute
+        cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)", (title, body, session['username']))
+
+        #commit to DB
+        mysql.connection.commit()
+
+        #close connection
+        cur.close()
+
+        flash('Article Created', 'success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_article.html', form=form)
+
+
 
 if __name__ == '__main__':
     app.secret_key ='pass' #set a secret key for password fail
